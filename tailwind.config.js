f7a83d18e078bb0b012d444f5f9ef7f4b10e0eb1
Zehra İkizler <zehraaikizler@gmail.com>
2023-01-12 01:03:34 +0300

    header-home-footer comp added

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      container: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+      colors: {
+        primary: "#3399ff",
+        secondary: "#e6f2ff",
+        success: "#063970",
+        danger: "#ff0000",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+        sans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
